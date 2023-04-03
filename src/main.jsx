b7b9d38e3bfe0a24c1cf73e424a1852d669cf503
ali1924 <mohammadali229987@gmail.com>
2023-04-04 04:52:47 +0600

    nested route system-1

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,31 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Error from './components/Error/Error';
+import Header from './components/Header/Header';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App></App>,
+    // errorElement:<Error></Error>
   },
+  {
+    path: '/about',
+    element:<About></About>
+  },
+  {
+    path: '/contact',
+    element:<Contact></Contact>
+  }
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header></Header>
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
