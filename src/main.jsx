72eaa4ef4f09896fe13b598265325dfc82f97b71
ali1924 <mohammadali229987@gmail.com>
2023-04-04 05:08:26 +0600

    54-2 (interesting) Nested route and outlet for shared content

--- src/main.jsx
+++ src/main.jsx
@@ -10,27 +10,44 @@ import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Error from './components/Error/Error';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>,
+//     // errorElement:<Error></Error>
+//   },
+//   {
+//     path: '/about',
+//     element:<About></About>
+//   },
+//   {
+//     path: '/contact',
+//     element:<Contact></Contact>
+//   }
+
+// ]);
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App></App>,
-    // errorElement:<Error></Error>
-  },
-  {
-    path: '/about',
-    element:<About></About>
-  },
-  {
-    path: '/contact',
-    element:<Contact></Contact>
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element:<About></About>
+      },
+      {
+        path: 'contact',
+        element:<Contact></Contact>
+      }
+    ]
   }
-
-]);
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header></Header>
+    {/* <Header></Header> */}
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
