424e2d363573764e85ab4225215479dd2ec53af0
ali1924 <mohammadali229987@gmail.com>
2023-04-04 14:04:01 +0600

    spinner usenavigation hook

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
             {/* <h3>This is real Home page</h3> */}
+            <div>
+                {navigation.state==='loading'?'Spinner Loading':' '}
+            </div>
             <Outlet></Outlet>
         </div>
     );
